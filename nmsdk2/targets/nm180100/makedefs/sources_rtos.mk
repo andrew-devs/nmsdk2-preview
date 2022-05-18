d15efdcdb76270caddc88456178165a04791f087
Joshua Wong <joshua@northernmechatronics.com>
2022-05-18 00:50:23 -0400

    Added build for RTOS.

--- nmsdk2/targets/nm180100/makedefs/sources_rtos.mk
+++ nmsdk2/targets/nm180100/makedefs/sources_rtos.mk
@@ -1,25 +1,25 @@
-DEFINES += -DAM_FREERTOS
+RTOS_DEFINES += -DAM_FREERTOS
 
-INCLUDES  = -I$(AMBIQ)/mcu/apollo3
-INCLUDES += -I$(AMBIQ)/CMSIS/AmbiqMicro/Include
-INCLUDES += -I$(AMBIQ)/CMSIS/ARM/Include
-INCLUDES += -I$(RTOS)/kernel/include
-INCLUDES += -I$(RTOS)/cli/include
-INCLUDES += -I./rtos/FreeRTOS
-INCLUDES += -I./rtos/FreeRTOS/portable
+RTOS_INC += -I$(HAL)/mcu/apollo3
+RTOS_INC += -I$(HAL)/CMSIS/AmbiqMicro/Include
+RTOS_INC += -I$(HAL)/CMSIS/ARM/Include
+RTOS_INC += -I$(RTOS)/kernel/include
+RTOS_INC += -I$(RTOS)/cli/include
+RTOS_INC += -I./rtos/FreeRTOS
+RTOS_INC += -I./rtos/FreeRTOS/portable
 
 VPATH += $(RTOS)/kernel
 VPATH += $(RTOS)/cli
 VPATH += ./rtos/FreeRTOS/portable
 
-SRC += croutine.c
-SRC += event_groups.c
-SRC += list.c
-SRC += queue.c
-SRC += stream_buffer.c
-SRC += tasks.c
-SRC += timers.c
-SRC += heap_4.c
-SRC += port.c
+RTOS_SRC += croutine.c
+RTOS_SRC += event_groups.c
+RTOS_SRC += list.c
+RTOS_SRC += queue.c
+RTOS_SRC += stream_buffer.c
+RTOS_SRC += tasks.c
+RTOS_SRC += timers.c
+RTOS_SRC += heap_4.c
+RTOS_SRC += port.c
 
-SRC += FreeRTOS_CLI.c
+RTOS_SRC += FreeRTOS_CLI.c
