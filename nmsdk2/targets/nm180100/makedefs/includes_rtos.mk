3f78fac7dd3cbd683773e835d5d150ab46905b91
Joshua Wong <joshua@northernmechatronics.com>
2022-05-18 23:33:50 -0400

    Updated the includes flag.

--- nmsdk2/targets/nm180100/makedefs/includes_rtos.mk
+++ nmsdk2/targets/nm180100/makedefs/includes_rtos.mk
@@ -2,6 +2,6 @@ RTOS_DEFINES += -DAM_FREERTOS
 
 RTOS_INC += -I$(RTOS)/kernel/include
 RTOS_INC += -I$(RTOS)/cli/include
-RTOS_INC += -I./rtos/FreeRTOS
-RTOS_INC += -I./rtos/FreeRTOS/portable
+RTOS_INC += -I$(RTOS)/../../targets/nm180100/rtos/FreeRTOS
+RTOS_INC += -I$(RTOS)/../../targets/nm180100/rtos/FreeRTOS/portable
 
