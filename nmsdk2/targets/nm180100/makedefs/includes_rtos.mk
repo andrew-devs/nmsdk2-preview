b6e197f7206cc9b50b180a00d16bf9c1f391e9cf
Joshua Wong <joshua@northernmechatronics.com>
2022-05-19 23:45:33 -0400

    Fixed FreeRTOSCLI include

--- nmsdk2/targets/nm180100/makedefs/includes_rtos.mk
+++ nmsdk2/targets/nm180100/makedefs/includes_rtos.mk
@@ -1,7 +1,7 @@
 RTOS_DEFINES += -DAM_FREERTOS
 
 RTOS_INC += -I$(RTOS)/kernel/include
-RTOS_INC += -I$(RTOS)/cli/include
+RTOS_INC += -I$(RTOS)/cli
 RTOS_INC += -I$(RTOS)/../../targets/nm180100/rtos/FreeRTOS
 RTOS_INC += -I$(RTOS)/../../targets/nm180100/rtos/FreeRTOS/portable
 
